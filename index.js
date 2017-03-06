7e02731370a5bc22540aceaa8208b1777a97d3e6
Alexander Alemayhu <alexander@alemayhu.com>
2017-03-07 00:03:10 +0100

    translate single lines
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var cmd = require('node-cmd');
 
 var app = express();
 
 app.use(express.static('public'))
+app.use(bodyParser.json());
 
 app.post('/translate', function (req, res) {
-  console.log(res);
-  res.send(req);
+  var rule = req.body.old_rules.replace("iptables ", "");
+  let translate_cmd = "iptables-translate "+rule;
+
+  console.log(translate_cmd);
+  cmd.get(translate_cmd, function(data) {
+    console.log(data);
+    res.send(data);
+  });
 });
 
 app.get('/version', function(req, res){
   cmd.get('iptables-translate --version', function(data) {
     res.send(data);
+  });
+});
+
+app.get('/help', function(req, res){
+  cmd.get('iptables-translate --help', function(data) {
+    res.send("<pre>"+data+"</pre>");
   });
 });
 
--- public/app.js
+++ public/app.js
@@ -1,11 +1,31 @@
-angular.module('2nft', [])
-  .controller('MainController', ['$scope', '$http', function ($scope, $http) {
-    $scope.iptables_version = "xyz";
-    $http({
-      method: 'GET',
-      url: '/version'
-    }).then(function successCallback(response) {
-	$scope.iptables_version = response.data;
-    }, function errorCallback(response) {
+let app = angular.module('2nft', []);
+
+let controller = app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+  $scope.old_rules_changed = function(obj, $event) {
+    let old_rules = obj.old_rules;
+    console.log(old_rules);
+
+    var req = {
+      method: 'POST',
+      url: '/translate',
+      headers: {
+	'Content-Type': 'application/json'
+      },
+      data: { old_rules: old_rules }
+    }
+    $http(req).then(function(res){
+      console.log('new'+res.data);
+      $scope.new_rules = res.data;
+    }, function(){
     });
-  }]);
+  };
+
+  $scope.iptables_version = "xyz";
+  $http({
+    method: 'GET',
+    url: '/version'
+  }).then(function successCallback(response) {
+    $scope.iptables_version = response.data;
+  }, function errorCallback(response) {
+  });
+}]);
