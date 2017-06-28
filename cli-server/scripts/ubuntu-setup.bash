0f28df0fa5090f692acf10bd46de3ed2e16e079c
Alexander Alemayhu <alexander@alemayhu.com>
2017-06-28 20:42:36 +0200

    server/{scripts/,Vagrantfile}: fix  issue detected while testing
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- cli-server/scripts/ubuntu-setup.bash
+++ cli-server/scripts/ubuntu-setup.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y golang
+sudo apt-get update
+sudo apt-get install -y golang
+sudo apt-get -y install docbook2x docbook-utils libgmp-dev libreadline-dev
+sudo apt-get install -y autoconf build-essential gcc make autoconf automake
+sudo apt-get install -y libjansson-dev pkg-config zlib1g-dev curl libtool
+sudo apt-get install -y docbook2x docbook-utils bison flex
 
-if [ -d "/home/ubuntu" ]; then
-  exit 0
-fi
-
-echo "export GOPATH="\$HOME"/opt/og" >> /home/ubuntu/.bashrc
+echo "export GOPATH="\$HOME"/opt/og" >> $HOME/.bashrc
