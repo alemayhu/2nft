c7fa1bb6de577ddd0d976b69e4cce1266d0837ce
Alexander Alemayhu <alexander@alemayhu.com>
2017-06-28 12:47:27 +0200

    server: add skeleton test for server
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ cli-server/server_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestTranslate(t *testing.T) {
+	t.Fatal("To be implemented")
+}
+
+func TestDownload(t *testing.T) {
+	t.Fatal("To be implemented")
+}
+
+func TestVersion(t *testing.T) {
+	t.Fatal("To be implemented")
+}
+
+func TestHelp(t *testing.T) {
+	t.Fatal("To be implemented")
+}
+
+func TestWerBinIch(t *testing.T) {
+	t.Fatal("To be implemented")
+}
+
